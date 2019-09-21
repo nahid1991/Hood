148537cc4a0c738a1711139a9047809a9e585073
Nahid <nahidshaiket10300@gmail.com>
2019-09-21 18:12:09 +0600

    Register VC ui started

---
+++ Hood/Controller/CreateAccountVC.swift
@@ -0,0 +1,17 @@
+//
+//  CreateAccountVC.swift
+//  Hood
+//
+//  Created by Nahid on 21/9/19.
+//  Copyright © 2019 Nahid. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
