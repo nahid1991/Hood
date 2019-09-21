5caf9d5c4e80df72f47202e4dbda6d8511ff2825
Nahid <nahidshaiket10300@gmail.com>
2019-09-21 17:23:20 +0600

    Login vc UI

---
+++ Hood/Controller/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVCViewController.swift
+//  Hood
+//
+//  Created by Nahid on 21/9/19.
+//  Copyright © 2019 Nahid. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Hood/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Hood
+//
+//  Created by Nahid on 21/9/19.
+//  Copyright © 2019 Nahid. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+let TO_LOGIN = "toLogin"
