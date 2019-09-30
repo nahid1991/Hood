19e71d7e2d4ad25cfd818fa904900b484c885cc9
Nahid <nahidshaiket10300@gmail.com>
2019-09-30 23:38:32 +0600

    tested register api

---
+++ Hood/Controllers/CreateAccntVC.swift
@@ -0,0 +1,42 @@
+//
+//  CreateAccntVC.swift
+//  Hood
+//
+//  Created by Nahid on 21/9/19.
+//  Copyright © 2019 Nahid. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccntVC: UIViewController {
+
+    @IBOutlet weak var userNameTxt: UITextField!
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var passwordTxt: UITextField!
+    @IBOutlet weak var userImg: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func closeButtonPressed(_ sender: Any) {
+        performSegue(withIdentifier: UNWIND, sender: nil)
+    }
+    
+    @IBAction func pickAvatarPressed(_ sender: Any) {
+    }
+    
+    @IBAction func pickBgColorPressed(_ sender: Any) {
+    }
+    
+    @IBAction func createAccntPressed(_ sender: Any) {
+        guard let email = emailTxt.text, emailTxt.text != "" else { return }
+        guard let password = passwordTxt.text, passwordTxt.text != "" else { return }
+        
+        AuthService.instance.registerUser(email: email, password: password) { (success) in
+            if success {
+                print("registered user!")
+            }
+        }
+    }
+}
---
+++ Hood/Views/RoundedButton.swift
@@ -0,0 +1,34 @@
+//
+//  RoundedButton.swift
+//  Hood
+//
+//  Created by Nahid on 30/9/19.
+//  Copyright © 2019 Nahid. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 3.0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.setupView()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        self.setupView()
+    }
+    
+    func setupView() {
+        self.layer.cornerRadius = cornerRadius
+    }
+    
+}
