dcdc0d7a0eca20a3da82b0b7af704498dc2720e7
Nahid <nahidshaiket10300@gmail.com>
2019-10-22 02:09:35 +0600

    working with channels

---
+++ Hood/Models/Channel.swift
@@ -0,0 +1,15 @@
+//
+//  Channel.swift
+//  Hood
+//
+//  Created by Nahid on 22/10/19.
+//  Copyright © 2019 Nahid. All rights reserved.
+//
+
+import Foundation
+
+struct Channel {
+    public private(set) var channelTitle: String!
+    public private(set) var channelDescription: String!
+    public private(set) var id: String!
+}
---
+++ Hood/Services/MessageService.swift
@@ -0,0 +1,47 @@
+//
+//  MessageService.swift
+//  Hood
+//
+//  Created by Nahid on 22/10/19.
+//  Copyright © 2019 Nahid. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+class MessageService {
+    
+    static let instance = MessageService()
+    
+    var channels = [Channel]()
+    
+    func findAllChannel(completion: @escaping CompletionHandler) {
+        Alamofire.request(URL_GET_CHANNELS, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: BEARER_HEADER).responseJSON { (response) in
+            if response.result.error == nil {
+                guard let data = response.data else {
+                    return
+                }
+                do {
+                    let json = try JSON(data: data).array
+                    for item in json! {
+                        let name = item["name"].stringValue
+                        let channelDescription = item["description"].stringValue
+                        let id = item["_id"].stringValue
+                        
+                        let channel = Channel(channelTitle: name, channelDescription: channelDescription, id: id)
+                        self.channels.append(channel)
+                    }
+                    completion(true)
+                } catch {
+                   completion(false)
+                   debugPrint("something went wrong!")
+                }
+            } else {
+                completion(false)
+                debugPrint(response.result.error as Any)
+            }
+        }
+    }
+    
+}
